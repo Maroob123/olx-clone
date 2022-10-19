04c88d73d70bd0614b27cc8c96819639e9d19e3f
Maroob123 <syedmuhammadmaroob123@gmail.com>
2022-10-20 04:28:24 +0500

    Minor Fixes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
 
   var firebaseConfig = {
-    apiKey: "AIzaSyA58m-YXKEm-xpIWRSmRqvJ-2aSSYUU35A",
-    authDomain: "olx-clone-with-reactjs.firebaseapp.com",
-    databaseURL: "https://olx-clone-with-reactjs.firebaseio.com",
-    projectId: "olx-clone-with-reactjs",
-    storageBucket: "olx-clone-with-reactjs.appspot.com",
-    messagingSenderId: "332487387082",
-    appId: "1:332487387082:web:0d79c0e568917e11569133",
-    measurementId: "G-C0W88HJQ54"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
